bfc26b26b76a252aca0a33c0b82128615128c540
SSR <ssrstudio99@gmail.com>
2020-09-02 16:11:13 +0530

    12-ObjectIntrospection.py

---
+++ 12.ObjectIntrospection.py
@@ -0,0 +1,28 @@
+class User():
+	def __init__(self,email):
+		self.email = email
+	
+	def sign_in(self):
+			print("Logged in")
+
+class Wizard(User):
+	def __init__(self,name,power,email):
+		super().__init__(email)
+		self.MyName = name
+		self.MyPower = power
+
+	def attack(self):
+		print(f"Attacking with power of {self.MyPower}")
+
+class Archer(User):
+	def __init__(self,name,num_of_arrows):
+		self.MyName = name
+		self.MyArrows = num_of_arrows
+
+	def attack(self):
+		print(f"Attacking with arrows: arrows left {self.MyArrows}")
+
+# Introspection
+wizard1 = Wizard("Merlin",60,"ssrstudio99@gmail.com")
+print(dir(wizard1))
+
